c0eaa4cba741367d44dbc48a51d3667a5a7c4f3d
Destroyerlll <75659889+Destroyerlll@users.noreply.github.com>
2020-12-08 18:40:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -103,24 +103,9 @@ module.exports = function gourd(mod) {
 		})
 		},
 		help() {
-		mod.command.message(`mg = Toggle on/off (it's on by default)`)
-		mod.command.message(`mg = Toggle on/off (it's on by default)`)
-		mod.command.message(`mg i (nickname) = inpect manual of the most recent history`)
-		mod.command.message(`mg hh (nickname) = Harrowhold`)
-		mod.command.message(`mg tshm (nickname) = Timescape Hard mode`)
-		mod.command.message(`mg tsnm (nickname) = Timescape Normal Mode`)
-		mod.command.message(`mg fanuw (nickname) = Forbidden Arena Nightmare Undying Warlord`)
-		mod.command.message(`mg fauw (nickname) = Forbidden Arena Undying Warlord`)
-		mod.command.message(`mg fa (nickname) = Forbidden Arena (Hagufna)`)
-		mod.command.message(`mg dahm (nickname) = Drakon Arena Hard Mode`)
-		mod.command.message(`mg danm (nickname) = Drakon Arena Normal Mode`)
-		mod.command.message(`mg cshm (nickname) = Corrupted Skynest Hard Mode`)
-		mod.command.message(`mg csnm (nickname) = Corrupted Skynest normal Mode`)
-		mod.command.message(`mg bahaar (nickname) = Bahaar's Sanctum`)
-		mod.command.message(`mg aq (nickname) = Akalath Quarantine`)
-		mod.command.message(`mg gvhm (nickname) = Gossamer Vault Hard Mode`)
-		mod.command.message(`mg aahm (nickname) = Antaroth's Abyss Hard Mode`)
-		mod.command.message(`mg glshm (nickname) = Grotto of Lost Souls Hard Mode`)		
+		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
+		link: encodeURI(`https://cdn.discordapp.com/attachments/732814199591272549/785982720546308116/unknown.png`)
+		})		
 		},		
 		i(name) {			
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
