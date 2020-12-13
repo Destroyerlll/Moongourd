a89ef6dbb8af57d862b4bfa61d38185907a0a332
Destroyerlll <75659889+Destroyerlll@users.noreply.github.com>
2020-12-13 01:26:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,77 +9,77 @@ module.exports = function gourd(mod) {
 		},
 		hh(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/950/4000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/950/4000/1/${name}`)
 		})
 		},
 		tshm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/456/1003/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/456/1003/1/${name}`)
 		})
 		},
 		fanuw(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3203/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3203/1000/1/${name}`)
 		})
 		},
 		fauw(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3103/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3103/1000/1/${name}`)
 		})
 		},
 		dahm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3202/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3202/1000/1/${name}`)
 		})
 		},
 		danm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3102/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3102/1000/1/${name}`)
 		})
 		},
 		cshm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3126/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3126/1000/1/${name}`)
 		})
 		},
 		csnm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3026/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3026/1000/1/${name}`)
 		})
 		},
 		fa(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3027/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3027/1000/1/${name}`)
 		})
 		},
 		bahaar(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/444/2000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/444/2000/1/${name}`)
 		})
 		},
 		AQ(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3023/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3023/1000/1/${name}`)
 		})
 		},
 		gvhm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/3201/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/3201/1000/1/${name}`)
 		})
 		},
 		aahm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/920/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/920/1000/1/${name}`)
 		})
 		},
 		glsh(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/982/1000/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/982/1000/1/${name}`)
 		})
 		},
 		tsnm(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/756/1003/1/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/756/1003/1/${name}`)
 		})
 		},
 		help() {
@@ -89,7 +89,7 @@ module.exports = function gourd(mod) {
 		},		
 		i(name) {			
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-		link: encodeURI(`https://moongourd.com/search/${name}?server=Velik`)
+		link: encodeURI(`https://moongourd.com/search/${name}`)
 		})		
 		}
 	}, this);
@@ -104,7 +104,7 @@ module.exports = function gourd(mod) {
 
 	function Open(name) {
 		mod.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
-			link: encodeURI(`https://moongourd.com/search/${name}?server=Velik`)
+			link: encodeURI(`https://moongourd.com/search/${name}`)
 		})
 	}
 }
